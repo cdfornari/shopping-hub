49fdaafc44e325821695b4e009f483d6dc4b38e0
Carlos Fornari <lilfornah@gmail.com>
2022-10-28 23:25:19 -0400

    BACKEND: remove imgs from server

--- backend/src/uploads/uploads.service.ts
+++ backend/src/uploads/uploads.service.ts
@@ -1,3 +1,4 @@
+import * as fs from 'node:fs/promises';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import {v2 as cloudinary } from 'cloudinary';
 
@@ -9,6 +10,7 @@ export class UploadsService {
             const {secure_url} = await cloudinary.uploader.upload(filePath,{
                 folder: 'shopping-hub'
             })
+            fs.unlink(filePath).catch(err => console.log(err))
             return secure_url;
         } catch (error) {
             console.log(error);
