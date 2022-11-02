80d38b2ceb8f758c4ee673fa61cc54d0d048d7c7
Alejandro <alejoguevarafm@gmail.com>
2022-11-02 12:38:53 -0400

    Se borro el helpers xd

--- admin/helpers/FilterProductsByStore.ts
+++
@@ -1,5 +0,0 @@
-import { Product } from '../../backend/dist/client/models/product';
-
-export const FilterProductsByStore = (storeId: string, products : Product[]) => {
-  return products.filter(p => p.store._id === storeId)
-}
